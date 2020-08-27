4e913c093e782787bfdbdb27942fa98aba260d12
ilitteri <ilitteri@fi.uba.ar>
2020-08-27 12:20:16 -0300

    DO NOT ADD /my-portfolio

---
+++ build/precache-manifest.df1e7beb03d41235bf6b5dc9bc0669ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86f0e0bda2b4e0322fd5",
+    "url": "/static/js/main.86f0e0bd.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "86f0e0bda2b4e0322fd5",
+    "url": "/static/css/main.5da25cdb.chunk.css"
+  },
+  {
+    "revision": "f306e050da083a93472132384ae58f67",
+    "url": "/index.html"
+  }
+];
