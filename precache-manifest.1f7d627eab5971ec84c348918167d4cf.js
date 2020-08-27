d211c0037bb3c7c9833d16dddfb23dda655da3d2
ilitteri <ilitteri@fi.uba.ar>
2020-08-26 23:05:03 -0300

    Updates

---
+++ precache-manifest.1f7d627eab5971ec84c348918167d4cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "80a858ea349ea81e507f",
+    "url": "/my-portfolio/static/js/main.80a858ea.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/my-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "80a858ea349ea81e507f",
+    "url": "/my-portfolio/static/css/main.5da25cdb.chunk.css"
+  },
+  {
+    "revision": "00706f7854b15f01eeba8afd131147fb",
+    "url": "/my-portfolio/index.html"
+  }
+];
