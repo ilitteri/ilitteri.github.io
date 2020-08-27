a47bca3db5b83330c3e84061bdd595c7a83ebdd5
ilitteri <ilitteri@fi.uba.ar>
2020-08-26 23:00:35 -0300

    Initial Commit

---
+++ build/precache-manifest.2e87a84f555c759e6ce6f21c5f6bf153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "818e331e07eda3d89ae7",
+    "url": "/my-portfolio/static/js/main.818e331e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/my-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "818e331e07eda3d89ae7",
+    "url": "/my-portfolio/static/css/main.f384bb95.chunk.css"
+  },
+  {
+    "revision": "f27c3d1a7cd55d224e77b017712bfdc0",
+    "url": "/my-portfolio/index.html"
+  }
+];
